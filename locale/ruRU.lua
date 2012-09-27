66619c44ae8e6d9ddf8d2817f21b1b76974c7c4d
Manriel <manriel@mail.ru>
2012-09-27 23:34:24 +0400

    Add locale files

---
+++ locale/ruRU.lua
@@ -0,0 +1,19 @@
+﻿if GetLocale() ~= "ruRU" then return nil end
+
+local addon, ns = ...
+local Locale = MyLocalizationTable;
+
+Locale = {
+	["Offline"] = "Не в сети",
+	["Dead"] = "Мертв",
+	["Ghost"] = "Призрак",
+	
+	["worldboss"] = "Босс",
+	["rareelite"] = "Редкая Элита",
+	["elite"] = "Элита",
+	["rare"] = "Редкий",
+	["normal"] = "Обычный",
+	["trivial"] = "Слабый",
+}
+
+ns.Locale = Locale
